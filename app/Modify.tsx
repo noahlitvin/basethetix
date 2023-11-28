94a64403ee7de2080969056911456f0021ba88ca
Peiman <peymannaderi3@yahoo.com>
2023-11-29 01:44:33 +0330

    update modify balance

--- app/Modify.tsx
+++ app/Modify.tsx
@@ -1,4 +1,4 @@
-import { AddIcon, MinusIcon } from "@chakra-ui/icons";
+import { AddIcon, MinusIcon } from '@chakra-ui/icons';
 import {
   Box,
   Button,
@@ -11,16 +11,19 @@ import {
   InputLeftElement,
   Heading,
   Text,
-} from "@chakra-ui/react";
-import type { NextComponentType } from "next";
-import { useState } from "react";
-import { useGetWithdrawable } from "../hooks/useGetWithdrawable";
+} from '@chakra-ui/react';
+import type { NextComponentType } from 'next';
+import { useState } from 'react';
+import { useGetWithdrawable } from '../hooks/useGetWithdrawable';
+import { useGetCollateral } from '../hooks/useGetCollateral';
 
 const Modify: NextComponentType<
   {},
   {},
   { isCollateral: boolean; account: string }
 > = ({ isCollateral, account }) => {
+  const { collateral } = useGetCollateral(account);
+
   const [isAdding, setIsAdding] = useState(true);
   const [isWithdrawalPending, setIsWithdrawalPending] = useState(true); // this is if there's collateral in the collateral that isn't delegated
   const [isWithdrawable, setIsWithdrawble] = useState(false); // this is if the above is true and the account timeout has passed
@@ -54,8 +57,14 @@ const Modify: NextComponentType<
     <>
       <Flex mb={4} flex={1}>
         <FormControl>
-          <Input px={0} readOnly type="text" value="420.69" border="none" />
-          <FormHelperText whiteSpace="nowrap" position="absolute">
+          <Input
+            px={0}
+            readOnly
+            type='text'
+            value={collateral?.toLocaleString() + ' USDC'}
+            border='none'
+          />
+          <FormHelperText whiteSpace='nowrap' position='absolute'>
             Wallet Balance: 0.00 USDC
           </FormHelperText>
         </FormControl>
@@ -64,29 +73,29 @@ const Modify: NextComponentType<
           <InputGroup>
             <InputLeftElement>
               <IconButton
-                size="xs"
-                colorScheme="blue"
-                aria-label="Add/Subtract"
+                size='xs'
+                colorScheme='blue'
+                aria-label='Add/Subtract'
                 icon={isAdding ? <AddIcon /> : <MinusIcon />}
                 onClick={() => setIsAdding(!isAdding)}
               />
             </InputLeftElement>
             <Input
-              type="number"
+              type='number'
               onChange={(e: any) => setAmount(e.target.value)}
             />
           </InputGroup>
         </FormControl>
 
-        <FormControl maxWidth="40px">
-          <Input readOnly type="text" value="=" border="none" py={0} />
+        <FormControl maxWidth='40px'>
+          <Input readOnly type='text' value='=' border='none' py={0} />
         </FormControl>
         <FormControl>
           <Input
             readOnly
-            type="text"
-            value="420.69 USDC"
-            border="none"
+            type='text'
+            value='420.69 USDC'
+            border='none'
             px={0}
           />
           <FormHelperText>0.00 USDC</FormHelperText>
@@ -94,12 +103,12 @@ const Modify: NextComponentType<
       </Flex>
       <Button
         isDisabled={amount == 0}
-        colorScheme="blue"
-        borderRadius="full"
-        w="100%"
-        mb="4"
+        colorScheme='blue'
+        borderRadius='full'
+        w='100%'
+        mb='4'
       >
-        {isAdding ? "Add" : "Remove"} {Math.abs(amount)} USDC
+        {isAdding ? 'Add' : 'Remove'} {Math.abs(amount)} USDC
       </Button>
 
       {isCollateral && isWithdrawalPending && (
@@ -107,23 +116,23 @@ const Modify: NextComponentType<
           mb={4}
           mt={2}
           pt={4}
-          borderTop="1px solid"
-          borderTopColor="gray.200"
+          borderTop='1px solid'
+          borderTopColor='gray.200'
         >
-          <Heading size="sm" mb={1}>
+          <Heading size='sm' mb={1}>
             Withdraw USDC
           </Heading>
-          <Text mb={3} fontSize="sm">
+          <Text mb={3} fontSize='sm'>
             As a temporary security precaution, you must wait 24 hours after
-            interacting with your account to withdraw USDC.{" "}
+            interacting with your account to withdraw USDC.{' '}
             {!isWithdrawable && <em>You have X hours remaining.</em>}
           </Text>
           {isWithdrawable ? (
-            <Button colorScheme="blue" borderRadius="full" w="100%">
+            <Button colorScheme='blue' borderRadius='full' w='100%'>
               Withdraw {withdrawable?.toLocaleString()} USDC
             </Button>
           ) : (
-            <Button isDisabled colorScheme="blue" borderRadius="full" w="100%">
+            <Button isDisabled colorScheme='blue' borderRadius='full' w='100%'>
               {withdrawable?.toLocaleString()} USDC Pending Withdrawal
             </Button>
           )}
