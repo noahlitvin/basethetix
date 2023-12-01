3f0dc9530ddd09769dc251166f54a41e5e32348a
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2023-12-01 17:52:13 -0500

    more

--- hooks/useGetWithdrawable.ts
+++ hooks/useGetWithdrawable.ts
@@ -1,21 +1,23 @@
-import { useAccount, useContractRead } from 'wagmi';
-import synthetix from '../deployments/system/CoreProxy.json';
-import usdc from '../deployments/usdc_mock_collateral/MintableToken.json';
-import { parseUnits } from '../utils/format';
+import { useAccount, useContractRead } from "wagmi";
+import synthetix from "../deployments/system/CoreProxy.json";
+import usdc from "../deployments/usdc_mock_collateral/MintableToken.json";
+import { parseUnits } from "../utils/format";
 
 export const useGetWithdrawable = (account: string | undefined) => {
   const { address, isConnected } = useAccount();
 
+  // TODO: This should actually be a call for sUSDC and sUSD and then add two values.
+
   const { data, isLoading, refetch } = useContractRead({
     address: synthetix.address as `0x${string}`,
     abi: synthetix?.abi,
-    functionName: 'getAccountAvailableCollateral',
+    functionName: "getAccountAvailableCollateral",
     args: [account, usdc.address],
     enabled: isConnected && !!address,
     watch: true,
   });
 
-  let withdrawableCollateral = parseUnits(data?.toString() || '0');
+  let withdrawableCollateral = parseUnits(data?.toString() || "0");
 
   return {
     withdrawable: withdrawableCollateral,
--- hooks/useWithdraw.ts
+++ hooks/useWithdraw.ts
@@ -2,9 +2,15 @@ export const useWithdraw = (account: string | undefined) => {
   /*
     Because of token approvals, it occurs to me we might want a smart contract that composes the calls with the spot market and the core system?
     need to think on it....
+    See useGetWithdrawable
 
-    withdrawable = useGetWithdrawable(account)
-    withdraw(account, sUsdcAddress, withdrawable) CORE SYSTEM https://github.com/Synthetixio/synthetix-v3/blob/main/protocol/synthetix/contracts/modules/core/CollateralModule.sol#L37
-    unwrap(uint128 marketId,withdrawable,withdrawable) SPOT MARKET https://github.com/Synthetixio/synthetix-v3/blob/main/markets/spot-market/contracts/modules/WrapperModule.sol#L48
+    withdrawablesUSD = getAccountAvailableCollateral with sUSDC
+    if(withdrawablesUSD > 0) {
+      buy sUSDC with the sUSD  SPOT MARKET https://github.com/Synthetixio/synthetix-v3/blob/main/markets/spot-market/contracts/modules/AtomicOrderModule.sol#L79
+    }
+
+    withdrawablesUSDC = getAccountAvailableCollateral
+    withdraw(account, sUsdcAddress, withdrawablesUSDC) CORE SYSTEM https://github.com/Synthetixio/synthetix-v3/blob/main/protocol/synthetix/contracts/modules/core/CollateralModule.sol#L37
+    unwrap(uint128 marketId,withdrawablesUSDC+withdrawablesUSD,withdrawablesUSDC+withdrawablesUSD) SPOT MARKET https://github.com/Synthetixio/synthetix-v3/blob/main/markets/spot-market/contracts/modules/WrapperModule.sol#L48
   */
 };
