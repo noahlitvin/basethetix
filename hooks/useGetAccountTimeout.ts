d8252353226d07ff549f91f776024addfa24e80c
Peiman <peymannaderi3@yahoo.com>
2023-12-04 18:42:22 +0330

    feat: update hooks

--- hooks/useGetAccountTimeout.ts
+++ hooks/useGetAccountTimeout.ts
@@ -1,4 +1,8 @@
-export const useAccountTimeout = (account: string | undefined) => {
+import { useContractRead } from 'wagmi';
+import synthetix from '../deployments/system/CoreProxy.json';
+import { useMemo } from 'react';
+
+export const useAccountTimeout = (accountId: string | undefined) => {
   /*
     const currentTimestamp = could get this from the RPC ideally, or just make something based on Date.now()
 
@@ -16,4 +20,29 @@ export const useAccountTimeout = (account: string | undefined) => {
     // For context, this is the relevant code. It's a countdown to when we could avoid this revert
     // https://github.com/Synthetixio/synthetix-v3/blob/main/protocol/synthetix/contracts/storage/Account.sol#L193
   */
+
+  const { data: accountLastInteraction } = useContractRead({
+    address: synthetix.address as `0x${string}`,
+    abi: synthetix?.abi,
+    functionName: 'getAccountLastInteraction',
+    args: [accountId],
+    watch: true,
+  });
+  const { data: accountTimeoutWithdraw } = useContractRead({
+    address: synthetix.address as `0x${string}`,
+    abi: synthetix?.abi,
+    functionName: 'getConfigUint',
+    args: [accountLastInteraction],
+    watch: true,
+    enabled: !!accountLastInteraction,
+  });
+
+  return useMemo(() => {
+    const timeSinceInteraction = Date.now() - Number(accountLastInteraction);
+
+    const timeUntilWithdrawal =
+      Number(accountTimeoutWithdraw) - timeSinceInteraction;
+
+    return Math.max(0, timeUntilWithdrawal);
+  }, [accountLastInteraction, accountTimeoutWithdraw]);
 };
--- hooks/useGetPnl.ts
+++ hooks/useGetPnl.ts
@@ -1,4 +1,8 @@
-export const useGetPnl = (account: string | undefined) => {
+import { useContractRead } from 'wagmi';
+import synthetix from '../deployments/system/CoreProxy.json';
+import usdc from '../deployments/usdc_mock_collateral/MintableToken.json';
+
+export const useGetPnl = (accountId: string | undefined) => {
   /*
   poolId = getPreferredPool() https://github.com/Synthetixio/synthetix-v3/blob/main/protocol/synthetix/contracts/interfaces/IPoolConfigurationModule.sol#L50;
   https://github.com/Synthetixio/synthetix-v3/blob/main/protocol/synthetix/contracts/interfaces/IVaultModule.sol#L91C14-L91C29
@@ -10,4 +14,22 @@ export const useGetPnl = (account: string | undefined) => {
 
   return this value, after multiplying by -1
 */
+
+  const { data: poolId } = useContractRead({
+    address: synthetix.address as `0x${string}`,
+    abi: synthetix?.abi,
+    functionName: 'getPreferredPool',
+    args: [],
+    watch: true,
+  });
+  const { data: debtD18 } = useContractRead({
+    address: synthetix.address as `0x${string}`,
+    abi: synthetix?.abi,
+    functionName: 'getPositionDebt',
+    args: [accountId, poolId, usdc.address],
+    enabled: !!poolId,
+    watch: true,
+  });
+
+  return debtD18;
 };
